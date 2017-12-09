5b0c394d4aff78b82bfba798571f0353f2fba58f
John Wojciechowski <jwojo13@gmail.com>
2017-12-09 17:15:51 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def add_song_by_name(song_name)
+  end
+
+  def self.song_count
+  end
+
+
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,10 @@
+class Author
+attr_accessor :name, :posts
+
+def initialize(name,posts)
+  @name = name
+  @post = []
+end
+
+
+end
